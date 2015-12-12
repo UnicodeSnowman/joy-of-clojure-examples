3b284de0e06cab9ec995ac124fe3deacf239ff5e
unicodesnowman <utfsnowman@gmail.com>
2015-12-12 15:15:58 -0500

    ch9 update

--- src/joy_of_clojure/9_combining_data_and_code.clj
+++ src/joy_of_clojure/9_combining_data_and_code.clj
@@ -16,3 +16,32 @@
 
 (in-ns 'your.ns)
 (clojure.core/prn joy.ns/authors)
+
+; 9.1.3 Declarative inclusions and exclusions
+
+(ns joy.ns-ex
+  (:refer-clojure :exclude [defstruct]) ; exclude defstruct from clojure.core
+  (:use (clojure set xml)) ; use everything in clojure.set and clojure.xml w/o qualification
+  (:use [clojure.test :only (are is)])
+  (:require (clojure [zip :as z]))
+  (:import (java.util Date)
+           (java.io File)))
+
+(def assoc-thing (assoc {:something 123} ::prototype 555))
+(::prototype assoc-thing)
+(find assoc-thing :other)
+(find assoc-thing :something)
+
+(do
+  (let [[a b] (find assoc-thing :something)]
+    (prn a)
+    (prn b)))
+
+; 9.3 Types, protocols, and records
+
+; Records
+(defrecord TreeNode [val l r])
+(def my-node (TreeNode. 5 nil nil))
+(:val my-node)
+(get my-node get my-node :val "NOPE")
+
