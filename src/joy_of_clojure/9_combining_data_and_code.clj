4b296c62a24624bead136de439227c99d30fb344
unicodesnowman <utfsnowman@gmail.com>
2015-12-14 00:43:28 -0500

    messing with records and protocols

--- src/joy_of_clojure/9_combining_data_and_code.clj
+++ src/joy_of_clojure/9_combining_data_and_code.clj
@@ -43,5 +43,37 @@
 (defrecord TreeNode [val l r])
 (def my-node (TreeNode. 5 nil nil))
 (:val my-node)
-(get my-node get my-node :val "NOPE")
+(get my-node :val "NOPE")
 
+(defn xconj [t v]
+  (cond
+    (nil? t) (TreeNode. v nil nil)
+    (< v (:val t)) (TreeNode. (:val t) (xconj (:l t) v) (:r t))
+    :else (TreeNode. (:val t) (:l t) (xconj (:r t) v))))
+
+(defn xseq [t]
+  (when t
+    (concat (xseq (:l t)) [(:val t)] (xseq (:r t)))))
+
+(def sample-tree
+  (reduce xconj nil [3 5 2 4 6]))
+(xseq sample-tree)
+
+(dissoc (TreeNode. 5 nil nil) :l) ; => returns a regular map
+
+; Protocols
+(defprotocol FIXO
+  (fixo-push [fixo value])
+  (fixo-pop [fixo])
+  (fixo-peek [fixo]))
+
+(extend-type TreeNode
+  FIXO
+  (fixo-push [node value]
+    (xconj node value)))
+
+(xseq (fixo-push sample-tree 5/2))
+(extend-type clojure.lang.IPersistentVector
+  FIXO
+  (fixo-push [vector value]
+    (conj vector value)))
