953786cb32e6399dbb07ccfe39e00c3f33410f76
unicodesnowman <utfsnowman@gmail.com>
2015-12-22 14:34:05 -0600

    moving on with ch10, getting into transactions

--- src/joy_of_clojure/10_mutation.clj
+++ src/joy_of_clojure/10_mutation.clj
@@ -1,4 +1,5 @@
-(ns joy-of-clojure.10-mutation)
+(ns joy-of-clojure.10-mutation
+  (:import java.util.concurrent.Executors))
 
 ; 10.1 When to use refs
 
@@ -38,3 +39,66 @@
 (def king-moves
   (partial neighbors
            [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]] 3))
+
+(defn good-move?
+  [to enemy-sq]
+  (when (not= to enemy-sq)
+    to))
+
+(defn choose-move
+  "Randomly choose a legal move"
+  [[[mover mpos] [_ enemy-pos]]]
+  [mover (some #(good-move? % enemy-pos)
+               (shuffle (king-moves mpos)))])
+
+(some #(good-move? % 1) [1 1 1 5]) ; returns first matching result, i.e. 5
+
+(reset-board!)
+(take 5 (repeatedly #(choose-move @to-move))) ; to-move is a ref, so we extract/de-ref with @
+
+; create a function to make a random move for the piece at the front of to-move
+(defn place [from to] to)
+
+(defn move-piece [[piece dest] [[_ src] _]]
+  (alter (get-in board dest) place piece)
+  (alter (get-in board src) place :-)
+  (alter num-moves inc))
+
+(defn update-to-move [move]
+  (alter to-move #(vector (second %) move))) ; fn passed to alter receives current value of the thing you are alter-ing
+
+(choose-move @to-move)
+
+(defn make-move []
+  (let [move (choose-move @to-move)] ; choose a random valid move for a piece
+    (dosync (move-piece move @to-move)) ; move it
+    (dosync (update-to-move move)))) ; update to-move to the other value (White to Black, Black to White, etc.), take turns
+
+(reset-board!)
+(make-move)
+(board-map deref board)
+
+(def thread-pool
+  (Executors/newFixedThreadPool
+    (+ 2 (.availableProcessors (Runtime/getRuntime)))))
+
+(defn dothreads!
+  [f & {thread-count :threads
+        exec-count   :times
+        :or {thread-count 1 exec-count 1}}]
+  (dotimes [t thread-count]
+    (.submit thread-pool
+             #(dotimes [_ exec-count] (f)))))
+
+(dothreads! #(.print System/out "Hi ") :threads 2 :times 2)
+
+(reset-board!)
+(dothreads! make-move :threads 100 :times 100) 
+(board-map deref board)
+; fuuuuuck this breaks shit... why?
+; Because! we use two separate dosync calls in make-move, giving us separate transactions which happen in different threads
+
+; but what exactly is a transaction? more specifically, what is software transactional memory?
+
+; 10.1.2
+
